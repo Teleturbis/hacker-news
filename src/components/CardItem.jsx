e03471c561cdc88e2c3d79be09c9ef8cc5d4e898
Olin Kirkland <me@olinkirk.land>
2022-02-28 16:42:40 +0100

    Add styles

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 export default function CardItem({ article }) {
-
-  if(!article.url){
-  console.log(article)
+  if (!article.url) {
+    // console.log(article);
   }
 
   return (
-    <li id={article.id}>
-      <a href={article.url} target="_blank">
-        <h2>{article.title}</h2>
-      </a>
-      <p className="articleDescription"></p>
+    <li className="article-card">
+      <div className="article-title-box">
+        <a className="article-title" href={article.url} target="_blank">
+          <h2>{article.title}</h2>
+        </a>
+      </div>
+      {/* <p className="articleDescription"></p> */}
       <p className="articleDetails">Points: {article.points}</p>
       <p className="articleDetails">Author: {article.author}</p>
       <p className="articleDetails">Created at: {article.date}</p>
