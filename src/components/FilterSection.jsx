46f08c8b7f57c48519013ab41dfb0a01a038c3b1
Kevin Poppe <kevin.poppe93@gmail.com>
2022-03-03 15:25:50 +0100

    styled few things

--- src/components/FilterSection.jsx
+++ src/components/FilterSection.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 export default function Filter({
-  sortAscendingArticles,
   sortDescendingArticles,
   searchForPost,
   sortNewestArticles,
@@ -9,12 +8,36 @@ export default function Filter({
   sortTrendingArticles,
 }) {
   const [userInput, setUserInput] = useState("");
+  const [newOldFilter, setNewOldFilter] = useState({mode: "newest", text: <i className="fa-solid fa-clock"> <i className="fa-solid fa-arrows-up-down"></i></i>})
 
   function searchPosts(e) {
     e.preventDefault();
 
     searchForPost(userInput);
   }
+
+  function toggleNewOldFilter(){
+
+    switch (newOldFilter.mode){
+
+      case "newest":
+        setNewOldFilter({mode: "oldest", text: <i className="fa-solid fa-clock"> <i className="fa-solid fa-arrow-up"></i></i>})
+        sortNewestArticles();
+        break;
+
+      case "oldest":
+        setNewOldFilter({mode: "newest", text: <i className="fa-solid fa-clock"> <i className="fa-solid fa-arrow-down-long"></i></i>})
+        sortOldestArticles();
+        break;
+
+      default:
+        break;
+
+    }
+
+  }
+
+                                  // <button onClick={sortOldestArticles}>Oldest</button>
 
   return (
     <div>
@@ -23,15 +46,15 @@ export default function Filter({
         value={userInput}
         placeholder="Suchen"
         onChange={(e) => setUserInput(e.target.value)}
+        className="searchTextInput"
       />
-      <input type="submit" value="Search" onClick={(e) => searchPosts(e)} />
+      <input className="searchButton" type="submit" value="Search" onClick={(e) => searchPosts(e)} />
 
-      <select name="filters">
-        <option onClick={sortDescendingArticles}>Points Descending</option>
-        <option onClick={sortNewestArticles}>Newest</option>
-        <option onClick={sortOldestArticles}>Oldest</option>
-        <option onClick={sortTrendingArticles}>Trending this Year</option>
-      </select>
+      <div name="filters" className="filters">
+        <button onClick={sortDescendingArticles} className="filterButton"><i className="fa-solid fa-thumbs-up"> <i className="fa-solid fa-arrow-up"></i></i></button>
+        <button onClick={toggleNewOldFilter} className="filterButton">{newOldFilter.text}</button>
+        <button onClick={sortTrendingArticles} className="filterButton"><i className="fa-brands fa-hotjar"></i></button>
+      </div>
     </div>
   );
 }
